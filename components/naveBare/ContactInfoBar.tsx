0a891893aa1a9f12567149d8268c5d20fb0d95fb
SPCODER-DEV <spcoder.dev@gmail.com>
2024-04-15 09:35:38 +0100

    .

---
+++ components/naveBare/ContactInfoBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ContactInfoBar() {
+  return (
+    <div className='flex px-2 cursor-pointer'>
+       <a href="tel:+075551235"><p className='font-bold'>075551235</p></a>
+    </div>
+  )
+}
